c3b5f6abbb8ad30b0987292dd7bea0de0f6f58ca
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-09-10 01:40:38 +0900

    upgrade : 削除ボタンの機能を調整
    削除ボタンを一つひとつのリストに、実装しました。

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@ let number = 1
 
 function addTodo() {
   if(!todoText.value) {
-    alert('入力してください。')
+    alert('Todoを入力してください。')
+    return
   }
   todos.value.push({
     id: number++,
@@ -17,29 +18,95 @@ function addTodo() {
   todoText.value = ''
 }
 
-function removeTodos() {
-  todos.value = todos.value.filter(todo => !todo.completed)
+function removeTodo(id) {
+  todos.value = todos.value.filter(todo => todo.id !== id)
 }
 </script>
 
 <template>
-<h1>ToDoアプリ</h1>
-<input type="text" v-model="todoText">
-<button @click="addTodo">追加する</button>
-<button @click="removeTodos">完了済みのものを削除</button>
+<h1>Todoアプリ</h1>
+<div class="todo-form">
+  <input type="text" v-model="todoText" class="add-input" placeholder="Todoを入力してください">
+  <button @click="addTodo" class="add-todo">追加する</button>
+</div>
 
-<p v-if="todos.length === 0">まだTodoがありません。</p>
-<ul v-else>
-  <li v-for="todo in todos" :key="todo.id">
-    <input type="checkbox" v-model="todo.completed">
-    <span :class="{ 'todo-completed': todo.completed }">{{ todo.text }}</span>
-  </li>
-</ul>
+<div class="todo-list">
+  <h2>Todo一覧</h2>
+  <p v-if="todos.length === 0">まだTodoがありません。</p>
+  <ul v-else>
+    <li v-for="todo in todos" :key="todo.id">
+      <span class="todo-desc">
+        <input type="checkbox" v-model="todo.completed">
+        <span :class="{ 'todo-completed': todo.completed }">{{ todo.text }}</span>
+      </span>
+      <button @click="removeTodo(todo.id)" class="todo-button">削除</button>
+    </li>
+  </ul>
+</div>
+
 
 </template>
 
 <style scoped>
+button {
+  cursor: pointer;
+}
+.todo-form {
+  display: flex;
+  align-items: center;
+}
+.todo-form + .todo-list {
+  margin-top: 60px;
+}
+.add-input {
+  padding: 10px 16px;
+  width: 100%;
+  border: 1px solid #ddd;
+  border-radius: 4px 0 0 4px;
+  line-height: 1.5;
+}
+.add-todo {
+  background-color: #f2f2f2;
+  border: 1px solid #ddd;
+  border-radius: 0 4px 4px 0;
+  border-left: none;
+  width: 100px;
+  min-width: 100px;
+  padding: 10px 16px;
+  position: relative;
+  line-height: 1.5;
+}
+
 .todo-completed {
   text-decoration: line-through;
 }
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  border-bottom: 1px solid #ddd;
+  padding-bottom: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 40px;
+}
+.todo-desc {
+  display: flex;
+  gap: 24px;
+}
+.todo-button {
+  background-color: #37e91c;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  padding: 2px 8px;
+  background-color: #e74c3c;
+  transition: .2s;
+}
+.todo-button:hover {
+  background-color: #c0392b;
+}
+
 </style>
